787e10c59f2e9e1f15e2ea0f43e31c9eeea817b7
Motyzhenkov Mikhail <m.motyzhenkov@qiwi.ru>
2019-11-06 15:36:04 +0300

    Update TextButton.swift

--- QiwiButtons/TextButton.swift
+++ QiwiButtons/TextButton.swift
@@ -11,7 +11,7 @@ import UIKit
 
 /// Button with text
 @IBDesignable @objc
-public class TextButton: TouchControllingView {
+open class TextButton: TouchControllingView {
     
     private(set) var heightConstraint: NSLayoutConstraint!
     
@@ -128,7 +128,7 @@ public class TextButton: TouchControllingView {
         commonInit()
     }
 
-    required init?(coder aDecoder: NSCoder) {
+	required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
